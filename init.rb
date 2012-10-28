d39aa144ed85450159fb3a7c42d1d42554d4c3ef
David S Anderson <ande3577@gmail.com>
2012-10-28 13:31:39 -0500

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_group_management do
+  name 'Redmine Group Management plugin'
+  author 'David Anderson'
+  description 'Allow a user to manage group memberships without admin priveleges.'
+  version '0.0.1'
+  url 'https://github.com/ande3577/redmine_group_management'
+  author_url 'https://github.com/ande3577/'
+end
